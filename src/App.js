21f8c51c13ab284891efd4083b0d3a7455bd15f0
Talia Pichardo <taliapichardo@gmail.com>
2023-06-22 13:48:17 -0400

    build html and css for donationCard

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import DonationForm from './components/donationForm/DonationForm';
+
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       Global Drop
+      <DonationForm />
     </div>
   );
 }
---
+++ src/components/donationForm/DonationForm.js
@@ -0,0 +1,33 @@
+import react from 'react';
+import './DonationForm.scss'
+
+const DonationForm = () => {
+    return (
+        <div className='donationForm'>
+            <div className="donationForm__title">
+                Choose an ammount to give
+            </div>
+            <div className="donationForm__form">
+                <div className="donationForm__amount donationForm__amount--selected">$40 <span>USD</span></div>
+                <div className="donationForm__amount">$100 <span>USD</span></div>
+                <div className="donationForm__amount">$240 <span>USD</span></div>
+                <div className="donationForm__amount">$500 <span>USD</span></div>
+                <div className="donationForm__amount">Other amount</div>
+                <button className="donationForm__submit">Give</button>
+            </div>
+            <div className="donationForm__promotion">
+                <div className="donationForm__icon">L</div>
+                <div>
+                    <div className='donationForm__promoTitle'>
+                        This is a matchmaking campaign!
+                    </div>
+                    <div className="donationForm__text">
+                        Every one-time gift will be matched by our longtime friends and supporters PCPartPicker.
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DonationForm;
