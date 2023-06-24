be29ee3b1874112436f82a053c35dd97abd085ae
Talia Pichardo <taliapichardo@Talias-MBP.lan1>
2023-06-24 18:48:50 -0400

    Add html and styling to Hero

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,22 @@
+import react from 'react';
+import DonationForm from '../donationForm/DonationForm';
+
+import './Hero.scss';
+
+const Hero = () =>{
+    return (
+        <div className='hero'>
+            <div className='hero__content'>
+                <h1>
+                    <span className='hero__callToAction'>Give Water.</span>
+                    <span className='hero__callToAction'>Give Life.</span>
+                </h1>
+                <div className='hero__donationForm'>
+                     <DonationForm/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
