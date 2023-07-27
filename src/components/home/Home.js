6eb5d7442af57c0d4e538ba88f9d9e63588745a3
Talia Pichardo <taliapichardo@gmail.com>
2023-07-27 12:31:29 -0400

    Edit our work based on change in focus to South Asian countries rather than countries around the world

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -12,18 +12,24 @@ const Home = () => {
         <div className='home'>
             <div className='home__ourWorkIntro'>
                 <span className='home__title'>Our Work</span>
-                <h2 className='home__callToActionHeader'>771 million people lack basic access to clean and safe drinking water. Join us in our mission to change that.</h2>
-                <p className='home__sustainableInitiativesIntro'>We strongly believe in the power of locally-led sustainable initiatives. Through collaborative efforts with our local partners, we not only establish community-owned water projects but also foster comprehensive water, sanitation, and hygiene programs. This holistic approach ensures the  protection of everyone’s long-term health.</p>
+                <h2 className='home__callToActionHeader'>385 million people lack basic access to clean and safe drinking water in South Asia. Join us in our mission to change that one community at a time.</h2>
+                <p className='home__sustainableInitiativesIntro'>We strongly believe in the power of locally-led sustainable initiatives. By working together with our local partners, we not only establish community-owned water projects but also foster comprehensive water, sanitation, and hygiene programs. This holistic approach ensures the  protection of everyone’s long-term health.</p>
             </div>
             <div className='home__ourWorkSvgs'>
                 <div className='home__numOfPeopleServed'>
                     <img src={peopleServedImg} alt="" />
+                    <h3>412</h3>
+                    <p>People served</p>
                 </div>
                 <div className='home__numOfFundedProjects'>
                     <img src={fundedProjectsImg} alt="" />
+                    <h3>10</h3>
+                    <p>Water projects funded</p>
                 </div>
                 <div className='home__numOfCountriesServed'>
                     <img src={countriesServedImg} alt="" />
+                    <h3>2</h3>
+                    <p>Countries</p>
                 </div>
             </div>
         </div>
