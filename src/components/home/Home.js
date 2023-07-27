b5d01e0a653dd396e4396ee0b16e7d7efcdc5cef
Talia Pichardo <taliapichardo@gmail.com>
2023-07-27 17:39:20 -0400

    Add stories mapout

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import OurWorkSvgs from '../ourWork/OurWorkSvgs';
 
+import whyWaterIntro from '../../assets/whyWaterIntro.jpg'
+
 import './Home.scss';
 
 const Home = () => {
@@ -12,9 +14,13 @@ const Home = () => {
                 <h2 className='home__callToActionHeader'>385 million people lack basic access to clean and safe drinking water in South Asia. Join us in our mission to change that one community at a time.</h2>
                 <p className='home__sustainableInitiativesIntro'>We strongly believe in the power of locally-led sustainable initiatives. By working together with our local partners, we not only establish community-owned water projects but also foster comprehensive water, sanitation, and hygiene programs. This holistic approach ensures the  protection of everyone’s long-term health.</p>
             </div>
-
             <OurWorkSvgs />
-
+            <div className=''>
+                <img src={whyWaterIntro} alt="two girls getting water from dirty pond" />
+                <h4>Clean water is a whole family concern.</h4>
+                <p>Finding water is a daily challenge for young girls, moms and sons. With a charitable donation today, you can lift this burden.</p>
+                <p>Providing a reliable and safe water source will unlock potential by returning time for study, work, and imagination.</p>
+            </div>
         </div>
     )
 }
